9e2e46277d72d95e22c57d642f07e9701815b7d0
Leuteris <leualexiou@hotmail.com>
2018-10-25 16:33:57 +0300

    First commit

--- app/src/main/java/com/apps/lefo/threadproject/MainActivity.java
+++ app/src/main/java/com/apps/lefo/threadproject/MainActivity.java
@@ -1,13 +1,42 @@
 package com.apps.lefo.threadproject;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startThread(View v){
+        ExampleThread thread = new ExampleThread(10);
+        thread.start();
+    }
+
+    class ExampleThread extends Thread{
+        int seconds;
+
+        public ExampleThread(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++){
+                Log.d(TAG, "run: " + i);
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
